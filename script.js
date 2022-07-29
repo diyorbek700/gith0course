65ac28b7ec3d2f114e980996f94725e15ccdf7ae
diyorbek <104645006+diyorbek700@users.noreply.github.com>
2022-07-29 09:47:47 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // FUNCTION DECLARATION
 
-function read(){
-    console.log('read');
-}
+// function read(){
+//     console.log('read');
+// }
 
 
 // FUNCTION EXPRESSION
-let write = function() {
-    console.log('write');
-};
+// let write = function() {
+//     console.log('write');
+// };
 
-let me2 = write;
-write();
-me2();
+// let me2 = write;
+// write();
+// me2();
+
+console.log('test2');
